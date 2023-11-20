7b793a15d26e511d9bb63c6ac212a610266f5a7d
Dmytro <moiseenkodmitriy@i.ua>
2023-11-20 21:52:32 +0200

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pic-finder',
+  base: '/Pic-finder',
   plugins: [react()],
 });
